188e44d2263213d391af7821cd6a37c1dcfcc550
mak <98904297+eafv@users.noreply.github.com>
2026-09-18 10:41:54 -0500

    Safer but lame

---
+++ Lessfun.bat
@@ -0,0 +1,13 @@
+@echo off
+start "" powershell -NoExit -Command ^
+    "$w=Add-Type -MemberDefinition '[DllImport(\"user32.dll\")]public static extern bool MoveWindow(IntPtr hWnd,int X,int Y,int nW,int nH,bool bR);' -Name 'W' -Namespace 'W' -PassThru;" ^
+    "$h=(Get-Process -Id $PID).MainWindowHandle;Clear-Host;Write-Host 'test';" ^
+    "$s=[System.Windows.Forms.Screen]::PrimaryScreen.Bounds;$wW=400;$wH=300;" ^
+    "$x=(Get-Random -Min 0 -Max ($s.Width-$wW));$y=(Get-Random -Min 0 -Max ($s.Height-$wH));" ^
+    "$dx=if((Get-Random -Min 0 -Max 2) -eq 0){10}else{-10};$dy=if((Get-Random -Min 0 -Max 2) -eq 0){10}else{-10};" ^
+    "while($true){" ^
+    "  $x+=$dx;$y+=$dy;" ^
+    "  if($x -le 0 -or $x+$wW -ge $s.Width){$dx=-$dx};if($y -le 0 -or $y+$wH -ge $s.Height){$dy=-$dy};" ^
+    "  [W.W]::MoveWindow($h,$x,$y,$wW,$wH,$True);" ^
+    "  Start-Sleep -Milliseconds 10;" ^
+    "}"
